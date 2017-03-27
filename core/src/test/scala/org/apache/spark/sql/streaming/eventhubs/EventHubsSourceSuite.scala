ee14076c275018ef83560f337cf17177f0b48153
Arijit Tarafdar <arijitt@live.com>
2017-03-27 10:40:36 -0700

    Add unit test for user-defined keys

--- core/src/test/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSourceSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSourceSuite.scala
@@ -40,7 +40,6 @@ abstract class EventHubsSourceTest extends StreamTest with SharedSQLContext {
 
 class EventHubsSourceSuite extends EventHubsSourceTest {
 
-
   testWithUninterruptibleThread("Verify expected offsets are correct") {
 
     val eventHubsParameters = Map[String, String](
@@ -82,14 +81,62 @@ class EventHubsSourceSuite extends EventHubsSourceTest {
     offset.targetSeqNums.values.foreach(x => assert(x == 2))
   }
 
-  testWithUninterruptibleThread("Verify dataframe schema is correct") {
+
+  testWithUninterruptibleThread("Verify expected dataframe size is correct") {
 
     val eventHubsParameters = Map[String, String](
       "eventhubs.policyname" -> "policyName",
       "eventhubs.policykey" -> "policyKey",
       "eventhubs.namespace" -> "ns1",
       "eventhubs.name" -> "eh1",
-      // "eventhubs.sql.userDefinedKeys" -> "creationTime",
+      "eventhubs.partition.count" -> "2",
+      "eventhubs.consumergroup" -> "$Default",
+      "eventhubs.progressTrackingDir" -> "/tmp",
+      "eventhubs.maxRate" -> s"10"
+    )
+
+    val eventPayloadsAndProperties = Seq(
+      1 -> Seq("propertyA" -> "a", "propertyB" -> "b", "propertyC" -> "c", "propertyD" -> "d",
+        "propertyE" -> "e", "propertyF" -> "f"),
+      0 -> Seq("propertyG" -> "g", "propertyH" -> "h", "propertyI" -> "i", "propertyJ" -> "j",
+        "propertyK" -> "k"),
+      3 -> Seq("propertyM" -> "m", "propertyN" -> "n", "propertyO" -> "o", "propertyP" -> "p"),
+      9 -> Seq("propertyQ" -> "q", "propertyR" -> "r", "propertyS" -> "s"),
+      5 -> Seq("propertyT" -> "t", "propertyU" -> "u"),
+      7 -> Seq("propertyV" -> "v")
+    )
+
+    val eventHubs = EventHubsTestUtilities.simulateEventHubs(eventHubsParameters,
+      eventPayloadsAndProperties)
+
+    val highestOffsetPerPartition = EventHubsTestUtilities.getHighestOffsetPerPartition(eventHubs)
+
+    val eventHubsSource = new EventHubsSource(spark.sqlContext, eventHubsParameters,
+      (eventHubsParams: Map[String, String], partitionId: Int, startOffset: Long, _: Int) =>
+        new TestEventHubsReceiver(eventHubsParams, eventHubs, partitionId, startOffset),
+      (_: String, _: Map[String, Map[String, String]]) =>
+        new TestRestEventHubClient(highestOffsetPerPartition))
+
+    val offset = eventHubsSource.getOffset.get.asInstanceOf[EventHubsBatchRecord]
+
+    val dataFrame = eventHubsSource.getBatch(None, offset)
+
+    assert(dataFrame.schema == eventHubsSource.schema)
+
+    eventHubsSource.commit(offset)
+
+    assert(dataFrame.select("body").count == 6)
+  }
+
+
+  testWithUninterruptibleThread("Verify user-defined keys show up in dataframe schema") {
+
+    val eventHubsParameters = Map[String, String](
+      "eventhubs.policyname" -> "policyName",
+      "eventhubs.policykey" -> "policyKey",
+      "eventhubs.namespace" -> "ns1",
+      "eventhubs.name" -> "eh1",
+      "eventhubs.sql.userDefinedKeys" -> "creationTime",
       "eventhubs.partition.count" -> "2",
       "eventhubs.consumergroup" -> "$Default",
       "eventhubs.progressTrackingDir" -> "/tmp",
@@ -123,6 +170,8 @@ class EventHubsSourceSuite extends EventHubsSourceTest {
     assert(dataFrame.schema == eventHubsSource.schema)
 
     eventHubsSource.commit(offset)
+
+    dataFrame.show()
 
     assert(dataFrame.select("body").count == 6)
   }

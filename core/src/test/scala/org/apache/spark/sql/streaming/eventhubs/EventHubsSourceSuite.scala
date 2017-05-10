a98200019560aa0b5b1ccf7697aa73485082f9ff
Nan Zhu <zhunansjtu@gmail.com>
2017-05-10 16:32:52 -0700

    test

--- core/src/test/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSourceSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSourceSuite.scala
@@ -678,11 +678,10 @@ class EventHubsSourceSuite extends EventHubsStreamTest {
       UpdatePartialCheck(
         EventHubsBatchRecord(0,
           Map(EventHubNameAndPartition("eh0", 1) -> 2, EventHubNameAndPartition("eh0", 0) -> 2))),
-      AdvanceManualClock(10),
       CheckAnswer(true, false, 7, 8, 9, 10, 11, 12),
       UpdatePartialCheck(
         EventHubsBatchRecord(1,
-          Map(EventHubNameAndPartition("eh0", 1) -> 5, EventHubNameAndPartition("eh0", 0) -> 5))),
+          Map(EventHubNameAndPartition("eh0", 1) -> 6, EventHubNameAndPartition("eh0", 0) -> 7))),
       AdvanceManualClock(10),
       CheckAnswer(7, 8, 9, 10, 11, 12, 13, 14, 15)
     )

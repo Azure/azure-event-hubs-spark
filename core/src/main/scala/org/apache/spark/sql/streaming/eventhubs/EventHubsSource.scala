c9eb2bca697a91f13e7e2614f64a017b1b73c446
Sabee Grewal <grewalsabee@gmail.com>
2017-11-28 15:04:18 -0800

    Databricks 3.4 (#214)
    * casting strings and maps to UTF8Strings and MapData
    
    * updating version numbers

--- core/src/main/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSource.scala
+++ core/src/main/scala/org/apache/spark/sql/streaming/eventhubs/EventHubsSource.scala
@@ -26,12 +26,15 @@ import org.apache.spark.eventhubs.common.rdd.{ EventHubsRDD, OffsetRange, Progre
 import org.apache.spark.eventhubs.common.{ EventHubsConnector, NameAndPartition, RateControlUtils }
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.util.ArrayBasedMapData
 import org.apache.spark.sql.execution.streaming.{ Offset, SerializedOffset, Source }
 import org.apache.spark.sql.streaming.eventhubs.checkpoint.StructuredStreamingProgressTracker
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{ DataFrame, SQLContext }
+import org.apache.spark.unsafe.types.UTF8String
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success }
 
@@ -251,6 +254,7 @@ private[spark] class EventHubsSource private[eventhubs] (
     import scala.collection.JavaConverters._
     val (containsProperties, userDefinedKeys) =
       EventHubsSourceProvider.ifContainsPropertiesAndUserDefinedKeys(eventHubsParams)
+
     val rowRDD = eventHubsRDD.map(
       eventData =>
         InternalRow.fromSeq(Seq(
@@ -258,24 +262,29 @@ private[spark] class EventHubsSource private[eventhubs] (
           eventData.getSystemProperties.getOffset.toLong,
           eventData.getSystemProperties.getSequenceNumber,
           eventData.getSystemProperties.getEnqueuedTime.getEpochSecond,
-          eventData.getSystemProperties.getPublisher,
-          eventData.getSystemProperties.getPartitionKey
+          UTF8String.fromString(eventData.getSystemProperties.getPublisher),
+          UTF8String.fromString(eventData.getSystemProperties.getPartitionKey)
         ) ++ {
           if (containsProperties) {
             if (userDefinedKeys.nonEmpty) {
               userDefinedKeys.map(k => {
-                eventData.getProperties.asScala.getOrElse(k, "").toString
+                UTF8String.fromString(eventData.getProperties.asScala.getOrElse(k, "").toString)
               })
             } else {
-              Seq(eventData.getProperties.asScala.map {
-                case (k, v) =>
-                  k -> (if (v == null) null else v.toString)
-              })
+              val keys = ArrayBuffer[UTF8String]()
+              val values = ArrayBuffer[UTF8String]()
+              for ((k, v) <- eventData.getProperties.asScala) {
+                keys.append(UTF8String.fromString(k))
+                if (v == null) values.append(null)
+                else values.append(UTF8String.fromString(v.toString))
+              }
+              Seq(ArrayBasedMapData(keys.toArray, values.toArray))
             }
           } else {
             Seq()
           }
         }))
+
     sqlContext.sparkSession.internalCreateDataFrame(rowRDD, schema, isStreaming = true)
   }
 

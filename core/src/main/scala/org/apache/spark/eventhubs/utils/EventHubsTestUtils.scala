a3a5a0a9a65788d769bfc2c4dc55d05ecc2019ca
Sabee Grewal <grewalsabee@gmail.com>
2018-06-05 19:05:15 -0700

    EventHubsTestUtils

--- core/src/main/scala/org/apache/spark/eventhubs/utils/EventHubsTestUtils.scala
+++ core/src/main/scala/org/apache/spark/eventhubs/utils/EventHubsTestUtils.scala
@@ -38,15 +38,30 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable
 
 /**
- * Test classes used to simulate an EventHubs instance.
+ * In order to better test the Event Hubs + Spark connector,
+ * this utility file has a simulated, in-memory Event Hubs
+ * instance. This doesn't fully simulate the Event Hubs
+ * service. Rather, it only simulates the the functionality
+ * needed to do proper unit and integration tests for the
+ * connector.
  *
- * In main directory (instead of test) so we can inject SimulatedClient
- * in our DStream and Source tests.
+ * This specific utility class provides the ability to create,
+ * send to, and destroy a simulated Event Hubs (as well as some
+ * other similar utility functions).
  */
 private[spark] class EventHubsTestUtils {
 
   import EventHubsTestUtils._
 
+  /**
+   * Sends events to the specified simulated event hub.
+   *
+   * @param ehName the event hub to send to
+   * @param partitionId the partition id that will receive the data
+   * @param data the data being sent
+   * @param properties additional application properties
+   * @return
+   */
   def send(ehName: String,
            partitionId: Option[PartitionId] = None,
            data: Seq[Int],
@@ -54,6 +69,14 @@ private[spark] class EventHubsTestUtils {
     eventHubs(ehName).send(partitionId, data, properties)
   }
 
+  /**
+   * Returns the latest sequence numbers for all partitions of the Event Hub
+   * specified in the provided [[EventHubsConf]].
+   *
+   * @param ehConf the configuration containing all Event Hub related info
+   * @return the latest sequence numbers for all partitions in the simulated
+   *         event hub
+   */
   def getLatestSeqNos(ehConf: EventHubsConf): Map[NameAndPartition, SequenceNumber] = {
     val n = ehConf.name
     (for {
@@ -62,23 +85,51 @@ private[spark] class EventHubsTestUtils {
     } yield NameAndPartition(n, p) -> seqNo).toMap
   }
 
+  /**
+   * Retrieves the simulated event hubs.
+   *
+   * @param ehName the name of the event hub
+   * @return the [[SimulatedEventHubs]] instance
+   */
   def getEventHubs(ehName: String): SimulatedEventHubs = {
     eventHubs(ehName)
   }
 
+  /**
+   * Creates a [[SimulatedEventHubs]].
+   *
+   * @param ehName the name of the simulated event hub
+   * @param partitionCount the number of partitions in the simulated event hub
+   * @return the newly created [[SimulatedEventHubs]]
+   */
   def createEventHubs(ehName: String, partitionCount: Int): SimulatedEventHubs = {
     EventHubsTestUtils.eventHubs.put(ehName, new SimulatedEventHubs(ehName, partitionCount))
     eventHubs(ehName)
   }
 
+  /**
+   * Destroys the the event hub if it is present.
+   * @param ehName the name of the simulated event hub to be destroyed.
+   */
   def destroyEventHubs(ehName: String): Unit = {
     eventHubs.remove(ehName)
   }
 
+  /**
+   * Destroys all simulated event hubs.
+   */
   def destroyAllEventHubs(): Unit = {
     eventHubs.clear
   }
 
+  /**
+   * Creates a generic [[EventHubsConf]] with dummy information.
+   *
+   * @param ehName the simulated event hub name to be used in this [[EventHubsConf]].
+   *               If this isn't correctly provided, all lookups for the simulated
+   *               event hubs will fail.
+   * @return the [[EventHubsConf]] with dummy information
+   */
   def getEventHubsConf(ehName: String = "name"): EventHubsConf = {
     val partitionCount = getEventHubs(ehName).partitionCount
 
@@ -100,7 +151,22 @@ private[spark] class EventHubsTestUtils {
       .setUseSimulatedClient(true)
   }
 
-  // Put 'count' events in every simulated EventHubs partition
+  /**
+   * Sends the same number of events to every partition in the
+   * simulated Event Hubs. All partitions will get identical
+   * data.
+   *
+   * Let's say we pass a count of 10. 10 events will be sent to
+   * each partition. The payload of each event is a simple
+   * counter. In this example, the payloads would be 0, 1, 2, 3,
+   * 4, 5, 6, 7, 8, 9.
+   *
+   * @param ehName the simulated event hub to receive the events
+   * @param count the number of events to be generated for each
+   *              partition
+   * @param properties the [[ApplicationProperties]] to be inserted
+   *                   to each event
+   */
   def populateUniformly(ehName: String,
                         count: Int,
                         properties: Option[Map[String, Object]] = None): Unit = {
@@ -111,6 +177,12 @@ private[spark] class EventHubsTestUtils {
   }
 }
 
+/**
+ * A companion class containing:
+ * - Constants used in testing
+ * - Simple utility methods
+ * - A mapping of Event Hub names to [[SimulatedEventHubs]] instances.
+ */
 private[spark] object EventHubsTestUtils {
   val DefaultPartitionCount: Int = 4
   val DefaultMaxRate: Rate = 5
@@ -146,6 +218,9 @@ private[spark] object EventHubsTestUtils {
 
 /**
  * Simulated EventHubs instance. All partitions are empty on creation.
+ *
+ * @param name the name of the [[SimulatedEventHubs]]
+ * @param partitionCount the number of partitions in the [[SimulatedEventHubs]]
  */
 private[spark] class SimulatedEventHubs(val name: String, val partitionCount: Int) {
 
@@ -154,10 +229,19 @@ private[spark] class SimulatedEventHubs(val name: String, val partitionCount: In
 
   private var count = 0
 
+  /**
+   * The number of events in a specific partition.
+   *
+   * @param partitionId the partition to be queried
+   * @return the number of events in the partition
+   */
   def partitionSize(partitionId: PartitionId): Int = {
     partitions(partitionId).size
   }
 
+  /**
+   * @return the number events in the entire [[SimulatedEventHubs]]
+   */
   def totalSize: Int = {
     var totalSize = 0
     for (part <- partitions.keySet) {

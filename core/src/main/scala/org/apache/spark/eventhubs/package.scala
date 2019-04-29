63cab73e18692cbe10c0fd4770725569e1f31217
SeongJoon Kwak (SJ) <sjkwak@microsoft.com>
2019-04-29 15:38:25 -0700

    Update version numbers for new release

--- core/src/main/scala/org/apache/spark/eventhubs/package.scala
+++ core/src/main/scala/org/apache/spark/eventhubs/package.scala
@@ -53,7 +53,7 @@ package object eventhubs {
   val EnqueuedTimeAnnotation = "x-opt-enqueued-time"
   val SequenceNumberAnnotation = "x-opt-sequence-number"
 
-  val SparkConnectorVersion = "2.3.10"
+  val SparkConnectorVersion = "2.3.11"
 
   type PartitionId = Int
   val PartitionId: Int.type = Int
